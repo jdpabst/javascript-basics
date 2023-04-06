533a46f69ba3217c54326215a0a4f2feac5185f7
User-1 <user-1@Pabst-Mac.local>
2023-04-06 15:46:59 -0600

    finished up to problem 12

--- practice.js
+++ practice.js
@@ -3,30 +3,34 @@
 //Create a variable called myName that is a string data type
 
 //Code here
+var myName = 'Jessica';
 
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
 //Code here
+var myAge = 29
 
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
 //Code here
+var lovesCode = true;
 
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
 //Code here
-
+var greatestFear = undefined;
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
 //Code here
+var devMountainGoal = null
 
 //////////////////PROBLEM 6////////////////////
 
@@ -36,21 +40,25 @@
 //plus the value of the name parameter.
 
 //Code here
-
+function greeting (name){
+  return 'Hello,' + ' ' + name;
+}
 //////////////////PROBLEM 7////////////////////
 
 //Write a function expression called newGreeting.
 //Give it the same functionality as the function greeting in Problem 6.
 
 //Code Here
-
+var newGreeting = function(name){
+  return 'Hello, ' + name;
+}
 //////////////////PROBLEM 8////////////////////
 
 //Create an array called groceries with the values
 //"apples", "milk", "eggs", "bread"
 
 //Code Here
-
+var groceries = ['apples', 'milk', 'eggs', 'bread'];
 //////////////////PROBLEM 9////////////////////
 
 //Create an object saved to the variable dog.
@@ -59,12 +67,18 @@
 //and goodBoy (a boolean).
 
 //Code Here
-
+var dog = {
+  name: "Beau",
+  color: "tan",
+  age: 15,
+  goodBoy: true
+}
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
 
 //Code Here
-
+var devMountainClassPet = dog.name;
+console.log(devMountainClassPet)
 //////////////////PROBLEM 10////////////////////
 
 //Write a function called nameCheck that takes in a name parameter.
@@ -75,7 +89,16 @@
 // with NAMEPARAM being the name parameter being passed in (not literally NAMEPARAM)
 
 // Code here
-
+function nameCheck(name){
+  if(name === 'Steven'){
+    return 'What is up Steven?';
+  } else if (name === 'Bryan'){
+    return 'Hey Bryan!';
+  } else {
+    return 'Cool name, ' + name;
+  }
+}
+console.log(nameCheck('Luna'));
 //////////////////PROBLEM 11////////////////////
 
 // Create a function called add that takes in two parameters
@@ -83,12 +106,14 @@
 // The add function should return the two parameters added together
 
 //Code Here
-
+function add(num1, num2){
+  return num1 + num2;
+}
 //Now invoke add, passing in the numbers 3 and 4
 //storing the result in the variable mathSum.
 
 //Code Here
-
+var mathSum = add(3, 4);
 //////////////////PROBLEM 12////////////////////
 
 //Write a function called faveColorFinder that takes in one parameter called color
@@ -99,7 +124,17 @@
 // Otherwise, you should return the string 'you need to evaluate your favorite color choice'
 
 // Code here
-
+function faveColorFinder(color){
+  if(color === 'red'){
+    return 'red is a great color';
+  } else if(color === 'green'){
+    return 'green is a solid favorite color';
+  } else if (color === 'black'){
+    return 'so trendy';
+  } else {
+    return 'you need to evaluate your favorite color choice';
+  }
+}
 //////////////////PROBLEM 13////////////////////
 
 let duck = "cute";
